0bd5c39b3cfd935d8c6b43d956d82d4cbb54355f
Savin <savin@angelmario.eu>
2023-03-23 16:05:41 +0200

    Updated gruvbox config

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,21 +1,26 @@
 vim.o.background = "dark"
 
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = false,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		string = true,
+		comments = true,
+		operators = false,
+		folds = false,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
 })
 
 vim.cmd('colorscheme gruvbox')
